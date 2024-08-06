34041bdf348e712b4b4869b2fe83857b16d9aa73
= <=>
2024-08-06 14:53:46 -0600

    add pythonpath shortcut in neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,9 +1,21 @@
+local function add_to_pythonpath(path)
+    local current_pythonpath = vim.fn.getenv 'PYTHONPATH'
+
+    if current_pythonpath ~= vim.NIL and current_pythonpath ~= '' then
+        current_pythonpath = current_pythonpath .. ':' .. path
+    else
+        current_pythonpath = path
+    end
+
+    vim.fn.setenv('PYTHONPATH', current_pythonpath)
+end
+
 return {
     'nvim-neo-tree/neo-tree.nvim',
     version = '*',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+        'nvim-tree/nvim-web-devicons',
         'MunifTanjim/nui.nvim',
     },
     cmd = 'Neotree',
@@ -14,7 +26,11 @@ return {
         filesystem = {
             window = {
                 mappings = {
-                    ['<leader>e'] = 'close_window',
+                    ['<C-p>'] = function(state)
+                        local path = state.tree:get_node().path
+                        print('Added to Python Path: ' .. path)
+                        add_to_pythonpath(path)
+                    end,
                 },
             },
         },
