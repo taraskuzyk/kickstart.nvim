febf7d6bb03de256692ccf7ebdf6b7a1614ab159
= <=>
2024-08-15 16:54:07 -0600

    wip dap configuration

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -50,15 +50,18 @@ return {
             automatic_installation = true,
             handlers = {},
 
-            -- You'll need to check that you have the required things installed
-            -- online, please don't ask me how to install them :)
             ensure_installed = {
-                -- Update this to ensure that you have the debuggers for the langs you want
-                'delve',
                 'debugpy',
             },
         }
-
+        table.insert(dap.configurations.python, {
+            type = 'python',
+            request = 'launch',
+            name = 'My custom launch configuration',
+            program = '${file}',
+            -- ... more options, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
+        })
+        require('dap-python').setup '~/.virtualenvs/debugpy/bin/python'
         -- Dap UI setup
         -- For more information, see |:help nvim-dap-ui|
         dapui.setup {
@@ -84,14 +87,5 @@ return {
         dap.listeners.after.event_initialized['dapui_config'] = dapui.open
         dap.listeners.before.event_terminated['dapui_config'] = dapui.close
         dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-        -- Install golang specific config
-        require('dap-go').setup {
-            delve = {
-                -- On Windows delve must be run attached or it crashes.
-                -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-                detached = vim.fn.has 'win32' == 0,
-            },
-        }
     end,
 }
