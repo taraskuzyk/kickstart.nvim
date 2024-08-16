f524e36c4b2073a3f2e6b074b12219edd469bca1
= <=>
2024-08-16 15:53:33 -0600

    remove cringe maps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -16,21 +16,3 @@ vim.keymap.set('n', '<C-Up>', ':resize -2<CR>')
 vim.keymap.set('n', '<C-Down>', ':resize +2<CR>')
 vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
 vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
-
--- Move text up and down
-vim.keymap.set('n', '<A-j>', '<Esc>:m .+1<CR>==gi')
-vim.keymap.set('n', '<A-k>', '<Esc>:m .-2<CR>==gi')
-
-vim.keymap.set('v', '<', '<gv')
-vim.keymap.set('v', '>', '>gv')
-
--- Move text up and down
-vim.keymap.set('v', '<A-j>', ':m .+1<CR>==')
-vim.keymap.set('v', '<A-k>', ':m .-2<CR>==')
-
--- Visual Block --
--- Move text up and down
-vim.keymap.set('x', 'J', ":move '>+1<CR>gv-gv")
-vim.keymap.set('x', 'K', ":move '<-2<CR>gv-gv")
-vim.keymap.set('x', '<A-j>', ":move '>+1<CR>gv-gv")
-vim.keymap.set('x', '<A-k>', ":move '<-2<CR>gv-gv")
