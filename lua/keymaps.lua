6034fadb059d0807f0fef5846fb40b3a84ba9159
Taras Kuzyk <taras.kuzyk@clio.com>
2025-03-28 11:19:53 -0600

    make resizing not conflict with MacOS (sigh)

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,7 +12,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('n', '<C-Up>', ':resize -2<CR>')
-vim.keymap.set('n', '<C-Down>', ':resize +2<CR>')
-vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
-vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
+vim.keymap.set('n', '<A-Up>', ':resize -2<CR>')
+vim.keymap.set('n', '<A-Down>', ':resize +2<CR>')
+vim.keymap.set('n', '<A-Left>', ':vertical resize -2<CR>')
+vim.keymap.set('n', '<A-Right>', ':vertical resize +2<CR>')
